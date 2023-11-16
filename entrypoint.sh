d7f8139105f0a6b0a1d0cc18de6cae2e5d0aed6c
Jake Livak <jake@inkarnate.com>
2023-11-15 22:45:16 -0700

    Restructured to avoid error exit.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,17 @@
 
 set -e
 
-if [[ -z "${ACTION_INPUTS_FILENAME}" ]] && [[ -z "${ACTION_INPUTS_FILENAMES}" ]] then
-  echo 'One or more file paths are required.,'
+# Escaping methods from https://stackoverflow.com/questions/29613304/is-it-possible-to-escape-regex-metacharacters-reliably-with-sed
+quoteSubst() {
+  IFS= read -d '' -r < <(sed -e ':a' -e '$!{N;ba' -e '}' -e 's/[&/\]/\\&/g; s/\n/\\&/g' <<<"$1")
+  printf %s "${REPLY%$'\n'}"
+}
+
+quoteRe() { sed -e 's/[^^]/[&]/g; s/\^/\\^/g; $!a\'$'\n''\\n' <<<"$1" | tr -d '\n'; }
+
+if [[ -z "${ACTION_INPUTS_FILENAME}" ]] && [[ -z "${ACTION_INPUTS_FILENAMES}" ]]
+then
+  echo 'One or more file paths are required.'
   exit 1
 else
   if [[ -z "${ACTION_INPUTS_FILENAME}" ]]
@@ -26,20 +35,17 @@ MIN_LENGTH=$(($MIN_LENGTH))
 while read -r -d "" var; do
   name="${var%%=*}"
   value="${var#*=}"
-  echo "Setting ${name} to ${value}."
   if [ ${#name} -ge $MIN_LENGTH  ] && [ "$name" != "ACTION_INPUTS_MIN_VARIABLE_LENGTH" ] && [ "$name" != "ACTION_INPUTS_FILENAME" ] && [ "$name" != "ACTION_INPUTS_FILENAMES" ]
   then
-    # Escaping methods from https://stackoverflow.com/questions/29613304/is-it-possible-to-escape-regex-metacharacters-reliably-with-sed
-
+    echo "Setting ${name} to ${value}."
     search="__${name}__"
-    searchEscaped=$(sed 's/[^^]/[&]/g; s/\^/\\^/g' <<<"$search")
+    searchEscaped=$(quoteRe "$search")
 
     replace=$value
-    IFS= read -d '' -r < <(sed -e ':a' -e '$!{N;ba' -e '}' -e 's/[&/\]/\\&/g; s/\n/\\&/g' <<<"$replace")
-    replaceEscaped=${REPLY%$'\n'}
+    replaceEscaped=$(quoteSubst "$replace")
 
     while read -r file; do
-      sed -i "s/$searchEscaped/$replaceEscaped/g" file
-    done < $FILENAMES
+      sed -i -e ':a' -e '$!{N;ba' -e '}' -e "s/${searchEscaped}/${replaceEscaped}/"  file
+    done < <($FILENAMES)
   fi
 done < <(printenv --null)
